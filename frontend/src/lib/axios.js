c2e4439f3ae0c40c2f3a525f7fe4f49b19448d9e
adarshuk02 <adarshuk79@gmail.com>
2024-12-20 10:02:48 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://chat-app-server-eight-red.vercel.app/api" : "https://chat-app-server-eight-red.vercel.app/api",
   withCredentials: true,
 });
